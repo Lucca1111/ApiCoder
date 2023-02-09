e48f9b20aecb9e28f41fce8983b64e45275f0d0a
Lucca1111 <82822662+Lucca1111@users.noreply.github.com>
2023-02-09 15:44:42 -0300

    Add files via upload

---
+++ Ado.Net/ManejadorVenta.cs
@@ -0,0 +1,39 @@
+﻿using ApiCoder.Modelos;
+using System.Data.SqlClient;
+
+namespace ApiCoder.Ado.Net
+{
+    internal static class ManejadorVenta
+    {
+
+        public static string connetionString = "Data Source =DESKTOP-RDML62O\\SQLEXPRESS;Initial Catalog=Cursoc#;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static List<Venta> ObtenerVentas(long idUsuario)
+        {
+            List<Venta> ventas = new List<Venta>();
+
+            using (SqlConnection conn = new SqlConnection(connetionString))
+            {
+                SqlCommand comando = new SqlCommand("SELECT * FROM Venta WHERE @IdUsuario = idUsuario", conn);
+                comando.Parameters.AddWithValue("@IdUsuario", idUsuario);
+                conn.Open();
+
+                SqlDataReader reader = comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Venta ventaTemp = new Venta();
+                        ventaTemp.Id = reader.GetInt64(0);
+                        ventaTemp.Comentarios = reader.GetString(1);
+                        ventaTemp.IdUsuario = reader.GetInt64(2);
+                        ventas.Add(ventaTemp);
+                    }
+                }
+
+                return ventas;
+
+            }
+        }
+    }
+}
+
